af61ce87b59a238e79e2affcfc7c8a6cf0b2bec5
Neha_Gupta <neha.gupta@intelligrape.com>
2016-02-26 15:09:05 +0530

    Added examples for method reference;

---
+++ src/com/methodreference/MethodReference.java
@@ -0,0 +1,28 @@
+package com.methodreference;
+
+
+public class MethodReference {
+    static void staticMethod() {
+        System.out.println("static method"); }
+    void instanceMethod() {
+        System.out.println("instance method");
+    }
+    MethodReference(){
+        System.out.println("constructor method");
+    }
+
+    public static void main(String[] args) {
+        //static method reference
+        MethodReferenceInterface staticMethodReference= MethodReference::staticMethod;
+        staticMethodReference.display();
+
+        //instance method reference
+        MethodReference methodRefrence=new MethodReference();
+        MethodReferenceInterface instanceMethodReference=methodRefrence::instanceMethod;
+        instanceMethodReference.display();
+
+        //constructor method reference
+        MethodReferenceInterface constructorMethodReference= MethodReference::new;
+        constructorMethodReference.display();
+    }
+}
